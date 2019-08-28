dbfd6278173a24c6c0461d53c481fe4e5e00fdc0
andmarek <andmarek@outlook.com>
2019-08-28 23:10:55 +0000

    fixed some CRUCIAl syntax errors

--- testfile.go
+++ testfile.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-    //    "time"
-    "errors"
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
-    "regexp"
+	//    "time"
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Semantics to use as delimiters more or less.
 const (
-    blogBeginSemantic = "$BEGIN$"
-    blogEndSemantic = "$FINISH$"
-    authorSemantic = "$AUTHOR$"
-    titleSemantic = "$TITLE$"
-    contentSemantic = "$CONTENT$"
+	blogBeginSemantic = "$BEGIN$"
+	blogEndSemantic   = "$FINISH$"
+	authorSemantic    = "$AUTHOR$"
+	titleSemantic     = "$TITLE$"
+	contentSemantic   = "$CONTENT$"
 
-    blogPrefix = "piece"
+	blogPrefix = "piece"
 )
 
-func getBlogFiles(dirPath string) (pathList []string, error) {
-    //Maybe "." because we want it to look at cur dir THEN the dir
-    err := filepath.Walk(".", func(dirPath, info.os.FileInfo, err error) error {
-      if err != nil {
-          fmt.Printf(//do something)
-          return err
-      }
-      if !(info.IsDir()) && info.Name().Contains(blogPrefix) {
-          //How to use Base?
-          pathList = append(info.Base())
-          //Remove later
-          fmt.Println(pathList)//
-      }
-    }
-    return pathList, nil
+func getBlogFiles(dirPath string) ([]string, error) {
+	//Maybe "." because we want it to look at cur dir THEN the dir
+	err := filepath.Walk(".", func(dirPath, f os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Ya'll got an error\n")
+			return err
+		}
+		if !(f.IsDir()) && info.Name().Contains(blogPrefix) {
+			//How to use Base?
+			pathList := append(info.Base())
+			//Remove later
+			fmt.Println(pathList) //
+		}
+	})
+	return pathList, nil
 }
 
 // parseBlogFile parses files in $BLOGDIR and puts them into instances of a
@@ -44,59 +44,47 @@ func getBlogFiles(dirPath string) (pathList []string, error) {
 // swap this out for a more efficient markdown parser or something, but this is
 // just something I hacked up in my spare time.
 func (bp *blog) parseBlogFile(path string) error {
-    file, err := os.Open("blogs/piece1")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer file.Close()
+	file, err := os.Open("blogs/piece1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
 
-    snr := bufio.NewScanner(file)
-    snr.Split(bufio.ScanLines)
+	snr := bufio.NewScanner(file)
+	snr.Split(bufio.ScanLines)
 
-    var s strings.Builder
+	var s strings.Builder
 
-    snr.Scan()
+	snr.Scan()
 
-    //Check if there's a BEGIN semantic
-    if(snr.Text() != "$BEGIN$") {
-        fmt.Printf("Not begin semantic\n")
-        err := errors.New("Begin semantic error!")
-        return err
-    }
+	//Check if there's a BEGIN semantic
+	if snr.Text() != "$BEGIN$" {
+		fmt.Printf("Not begin semantic\n")
+		err := errors.New("Begin semantic error!")
+		return err
+	}
 
-    for snr.Scan() {
-        if snr.Text() == blogBeginSemantic{
-            continue
-        }
-        if snr.Text() == authorSemantic {
-            snr.Scan()
-            bp.Author = snr.Text()
-        }
-        if snr.Text() == titleSemantic {
-            snr.Scan()
-            bp.Title = snr.Text()
-        }
-        if snr.Text() == contentSemantic {
-            for snr.Scan() {
-                if snr.Text() == blogEndSemantic {
-                    break
-                }
-                s.Write(snr.Bytes())
-            }
-            bp.Post = s.String()
-        }
-    }
-    return nil
-}
-
-//Take any file that is not a directory
-func walkBlogFile(path string, info.os.FileInfo, err error) error {
-    if err != nil {
-        fmt.Printf("handle failure accessing path %q: %v\n", path, err)
-        return err
-    }
-
-    if !(info.IsDir()) {
-
-    }
+	for snr.Scan() {
+		if snr.Text() == blogBeginSemantic {
+			continue
+		}
+		if snr.Text() == authorSemantic {
+			snr.Scan()
+			bp.Author = snr.Text()
+		}
+		if snr.Text() == titleSemantic {
+			snr.Scan()
+			bp.Title = snr.Text()
+		}
+		if snr.Text() == contentSemantic {
+			for snr.Scan() {
+				if snr.Text() == blogEndSemantic {
+					break
+				}
+				s.Write(snr.Bytes())
+			}
+			bp.Post = s.String()
+		}
+	}
+	return nil
 }
--- testmain.go
+++ testmain.go
@@ -1,34 +1,26 @@
 package main
 
-
 import (
-    "fmt"
+	"fmt"
 )
-func myWalkFn(path string, info os.FileInfo, err error) error {
-  if err != nil {
-      fmt.Printf("Could not walk filepath %s\n Maybe it doesn't exist?", path)
-      return err
-  }
-  if !(info.isDir()) {
 
-  }
-}
+func main() {
+	paths, _ := getBlogFiles(".")
+	tbs := blogSite{}
+	//Correct syntax?
+	for i := range paths {
+		parseBlogFile(paths[i])
+	}
+	//Print our paths to test it
+	fmt.Println(paths)
+	//test blog site
+	//create empty blogsite
+	tbs.Name = "Divided We Stand"
+	//adds new blog to the tbs
+	tbs.NewBlog()
 
-func main () {
-    paths := getBlogFiles(".")
-    tbs := blogSite{}
-    //Correct syntax?
-    for i in range paths {
-        parseBlogFiles(paths[i])
-    }
-    //test blog site
-    //create empty blogsite
-    tbs.Name = "Divided We Stand"
-    //adds new blog to the tbs
-    tbs.NewBlog()
-
-    fmt.Println((*tbs.Blogs[0]).Title)
-    fmt.Println((*tbs.Blogs[0]).Author)
-    fmt.Println((*tbs.Blogs[0]).Post)
+	fmt.Println((*tbs.Blogs[0]).Title)
+	fmt.Println((*tbs.Blogs[0]).Author)
+	fmt.Println((*tbs.Blogs[0]).Post)
 
 }
