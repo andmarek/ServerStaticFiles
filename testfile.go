50a22aaccda8833d2f596edba4080d4177f87388
andmarek <andmarek@outlook.com>
2019-08-28 22:41:39 -0500

    func to return pathlist

--- testfile.go
+++ testfile.go
@@ -1,27 +1,49 @@
 package main
 
 import (
-//    "time"
+    //    "time"
     "errors"
     "bufio"
     "fmt"
     "os"
     "strings"
+    "regexp"
 )
 
 // Semantics to use as delimiters more or less.
-const blogBeginSemantic = "$BEGIN$"
-const blogEndSemantic = "$FINISH$"
-const authorSemantic = "$AUTHOR$"
-const titleSemantic = "$TITLE$"
-const contentSemantic = "$CONTENT$"
+const (
+    blogBeginSemantic = "$BEGIN$"
+    blogEndSemantic = "$FINISH$"
+    authorSemantic = "$AUTHOR$"
+    titleSemantic = "$TITLE$"
+    contentSemantic = "$CONTENT$"
+
+    blogPrefix = "piece"
+)
+
+func getBlogFiles(dirPath string) (pathList []string, error) {
+    //Maybe "." because we want it to look at cur dir THEN the dir
+    err := filepath.Walk(".", func(dirPath, info.os.FileInfo, err error) error {
+      if err != nil {
+          fmt.Printf(//do something)
+          return err
+      }
+      if !(info.IsDir()) && info.Name().Contains(blogPrefix) {
+          //How to use Base?
+          pathList = append(info.Base())
+          //Remove later
+          fmt.Println(pathList)//
+      }
+    }
+    return pathList, nil
+}
 
 // parseBlogFile parses files in $BLOGDIR and puts them into instances of a
 // 'blog' struct.  The blog file follows a particular, and rather simple format
 // to make parsing easier.  This may be revamped for future use and maybe I'll
 // swap this out for a more efficient markdown parser or something, but this is
 // just something I hacked up in my spare time.
-func (bp *blog) parseBlogFile() error {
+func (bp *blog) parseBlogFile(path string) error {
     file, err := os.Open("blogs/piece1")
     if err != nil {
         fmt.Println(err)
@@ -40,8 +62,6 @@ func (bp *blog) parseBlogFile() error {
         fmt.Printf("Not begin semantic\n")
         err := errors.New("Begin semantic error!")
         return err
-    } else  {
-        fmt.Printf("Found being semantic\n")
     }
 
     for snr.Scan() {
@@ -67,4 +87,16 @@ func (bp *blog) parseBlogFile() error {
         }
     }
     return nil
+}
+
+//Take any file that is not a directory
+func walkBlogFile(path string, info.os.FileInfo, err error) error {
+    if err != nil {
+        fmt.Printf("handle failure accessing path %q: %v\n", path, err)
+        return err
+    }
+
+    if !(info.IsDir()) {
+
+    }
 }
